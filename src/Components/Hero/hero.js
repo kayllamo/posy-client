9b7aca53e2ac7a35838584cd4c855b697eb32327
Kayla Graham <kaylagraham@Kaylas-MBP.attlocal.net>
2020-01-09 19:38:09 -0800

    updates to contrast, favicon, added semi colon, and updated snapshots of tests

--- src/Components/Hero/hero.js
+++ src/Components/Hero/hero.js
@@ -16,23 +16,31 @@ class Hero extends Component {
             
             <div className='hero'>
                 <div className='herotext'>
+                    <div className='contrastBox'>
                     <h1 className='heroTitle'>POSY</h1>
                     <h2 className='heroTag'>positivity in your pocket</h2>
+                    </div>
                 </div>
                 <Link className='herolinks' to='/register'>Register</Link>
             </div>
 
             <div className='about'>
                 <div className='aboutInner1'>
+                <div className='contrastBox'>
                     <h4 className='aboutTag'>Let your thoughts flow</h4>
+                    </div>
                 </div>
 
                 <div className='aboutInner2'>
+                <div className='contrastBox'>
                     <h4 className='aboutTag'>Clear your mind</h4>
+                    </div>
                 </div>
 
                 <div className='aboutInner3'>
+                <div className='contrastBox'>
                     <h4 className='aboutTag'>Create healthy habits</h4>
+                    </div>
                 </div>
             </div>
 
